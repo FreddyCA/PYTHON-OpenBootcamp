fe8a27a639c5ff0732fa7ef2c429f9b5ad4d9e3b
Freddy Coaricona <fcoaricona@outlook.com>
2022-10-10 19:17:39 -0400

    ejercicio 5

--- ejercicio5/ejercicio5.py
+++ ejercicio5/ejercicio5.py
@@ -1,10 +1,12 @@
 ##Escribe una función que pueda decirte si un año (número entero) es bisiesto o no.
 def bisiesto(year):
-    if year % 4 == 0:
-        print("es divisible entre 4")
-        if year % 100 == 0:
-            print("es divisible entre 100")
-            if year % 400 == 0:
-                print("es divisible entre 400")
+    if year % 400 == 0:
+        print(year, " SI es un año bisiesto")
+    elif year % 100 != 0 and year % 4 == 0:
+        print(year, " SI es un año bisiesto")
+    else:
+        print(year, " NO es un año bisiesto")
 
-bisiesto(2008)
+bisiesto(2020)
+bisiesto(1999)
+bisiesto(2000)
